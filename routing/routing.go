74017e59b5919f677c144847be9856cb097660d3
dcmattyg <mgarrett0402@gmail.com>
2019-01-22 10:51:22 -0800

    Fixed CORS middleware location

--- routing/routing.go
+++ routing/routing.go
@@ -30,19 +30,6 @@ var Router *chi.Mux
 
 func init() {
 	Router = chi.NewRouter()
-
-	cors := cors.New(cors.Options{
-		// AllowedOrigins: []string{"https://foo.com"}, // Use this to allow specific origin hosts
-		AllowedOrigins: []string{"*"},
-		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
-		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
-		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
-		ExposedHeaders:   []string{"Link"},
-		AllowCredentials: true,
-		MaxAge:           300, // Maximum value not ignored by any of major browsers
-	})
-
-	Router.Use(cors.Handler)
 }
 
 // Routers Function
@@ -116,6 +103,19 @@ func Routers() {
 			}
 		}
 	}
+
+	cors := cors.New(cors.Options{
+		// AllowedOrigins: []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	})
+
+	Router.Use(cors.Handler)
 }
 
 // ParseVars function
