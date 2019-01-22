66c4f4688e390020a199085ddd09adf7e67d4df7
dcmattyg <mgarrett0402@gmail.com>
2019-01-22 10:02:25 -0800

    Fixed minor error in listWorkflow

--- workflow/workflow.go
+++ workflow/workflow.go
@@ -89,7 +89,7 @@ func ListWorkflows(w http.ResponseWriter, r *http.Request) {
 
 	foundVal, foundErr := database.FindAll("workflow", "workflows", primitive.M{})
 
-	if foundErr != nil {
+	if foundErr == nil {
 		fmt.Println(foundErr)
 		util.RespondWithError(w, http.StatusBadRequest, "no workflows found")
 		return
