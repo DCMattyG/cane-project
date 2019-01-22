80957b0674b385fd46f704bd678e6de9dbd4125f
dcmattyg <mgarrett0402@gmail.com>
2019-01-22 10:44:05 -0800

    Added CORS

--- routing/routing.go
+++ routing/routing.go
@@ -18,6 +18,7 @@ import (
 	"strings"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/go-chi/jwtauth"
 	"github.com/mitchellh/mapstructure"
 	"github.com/mongodb/mongo-go-driver/bson/primitive"
@@ -29,6 +30,19 @@ var Router *chi.Mux
 
 func init() {
 	Router = chi.NewRouter()
+
+	cors := cors.New(cors.Options{
+		// AllowedOrigins: []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	})
+
+	Router.Use(cors.Handler)
 }
 
 // Routers Function
